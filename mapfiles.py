0a756fb116df15252be207aa97a837f2cfb30bd0
Aidan Hobson Sayers <aidanhs@cantab.net>
2014-11-28 23:49:03 +0000

    Less tightly coupled datafile call generation for .py files

--- mapfiles.py
+++ mapfiles.py
@@ -6,24 +6,43 @@ import py_compile
 
 BASEDIR = '/usr/local/lib/python2.7'
 
-def mk_file(dname, fname, target):
-    fpath = os.path.join(dname, fname)
-    if fname.endswith('.py'):
+def files_to_datafilecalls(fpaths):
+    commands = []
+    dpaths = set([BASEDIR])
+    for fpath, targetdir in fpaths:
+
+        assert targetdir[0] == '.'
+        if not fpath.endswith('.py'):
+            continue
+        dpath = os.path.abspath(os.path.join(BASEDIR, targetdir))
+
         # We compile to save space and time in the parser
         py_compile.compile(fpath, fpath + 'c')
         fpath += 'c'
-        fname += 'c'
-    else:
-        return ''
-    contents = ','.join(str(ord(i)) for i in open(fpath, 'rb').read())
-    return 'FS.createDataFile("%s", "%s", [%s], true, true);' % (target, fname, contents)
+        contents = ','.join(str(ord(i)) for i in open(fpath, 'rb').read())
+        commands.append('FS.createDataFile("%s", "%s", [%s], true, true);' % (
+            dpath, os.path.basename(fpath), contents
+        ))
+
+        # Make sure we're adding all required directories
+        while dpath not in dpaths:
+            dpaths.add(dpath)
+            dpath = os.path.dirname(dpath)
+
+    dpaths.remove(BASEDIR)
+    for dpath in sorted(dpaths, key=len, reverse=True):
+        commands.insert(0, 'FS.createFolder("%s", "%s", true, true);' % (
+            os.path.dirname(dpath), os.path.basename(dpath)
+        ))
+    commands.insert(0, 'FS.createPath("/", "' + BASEDIR[1:] + '", true, true);')
+
+    return commands
 
 def main(root):
     os.chdir(root)
     # http://bugs.python.org/issue22689
     #commands = ['ENV["PYTHONHOME"] = "%s";' % (BASEDIR,)]
-    commands = []
-    commands.append('FS.createPath("%s", "%s", true, true);' % ('/', BASEDIR[1:]))
+    fpaths = []
     for (dirpath, dirnames, filenames) in os.walk('.'):
         for dirname in dirnames[:]:
             should_remove = any([
@@ -45,14 +64,13 @@ def main(root):
             if should_remove:
                 dirnames.remove(dirname)
 
-        target = os.path.abspath(os.path.join(BASEDIR, dirpath))
-        for dname in dirnames:
-            commands.append('FS.createFolder("%s", "%s", true, true);' % (target, dname))
         for filename in filenames:
-            commands.append(mk_file(dirpath, filename, target))
+            fpaths.append((os.path.join(dirpath, filename), dirpath))
 
     # _sysconfigdata is created by the build process
-    commands.append(mk_file('../build/lib.linux-x86_64-2.7', '_sysconfigdata.py', BASEDIR))
+    fpaths.append(('../build/lib.linux-x86_64-2.7/_sysconfigdata.py', '.'))
+
+    commands = files_to_datafilecalls(fpaths)
 
     # Start out in a writeable folder.
     commands.append('FS.createFolder(".", "sandbox", true, true);')
